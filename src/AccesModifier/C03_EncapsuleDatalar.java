6053681474481580be3708039106e4c3d903b78d
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-05-04 04:33:31 +0300

    04/05/2023

---
+++ src/AccesModifier/C03_EncapsuleDatalar.java
@@ -0,0 +1,21 @@
+package AccesModifier;
+
+public class C03_EncapsuleDatalar {
+    static private String isim="Yildiz Pazarlama";
+
+    private int satisTutari;
+    public int toplamSatis;
+
+    public int getToplamSatis() {
+        return toplamSatis;
+    }
+
+    public static String getIsim() {
+        return isim;
+    }
+
+    public void setSatisTutari(int satisTutari) {
+        this.satisTutari = satisTutari;
+        toplamSatis+=satisTutari;
+    }
+}
