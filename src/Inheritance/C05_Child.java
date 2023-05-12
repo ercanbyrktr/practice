47283911e65595881284ede49e0af8bc2274600a
Ercan Bayraktar <ercan.byrktr.545435@gmail.com>
2023-05-12 23:13:52 +0300

    12/05/2023

---
+++ src/Inheritance/C05_Child.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+public class C05_Child {
+    int c=20;
+
+    int a;
+    String s="Child class";
+    String m;
+    public void method1(){
+        System.out.println("Child class method1");
+    }
+
+    public static void main(String[] args) {
+        C05_Child obj=new C05_Child();
+        System.out.println(obj.a);
+        System.out.println(obj.c);//20
+        //System.out.println(obj.b);//0
+        System.out.println(obj.m);//null
+        System.out.println(obj.s);//Child class
+        //System.out.println(obj.t);//null
+        obj.method1();//Child class method1
+       // obj.method2();//parent class method2
+
+
+    }
+}
